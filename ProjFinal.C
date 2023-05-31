fb509be813c7296981746852317b5805657d41cd
Luana <135066206+luayasuda@users.noreply.github.com>
2023-05-31 16:02:54 -0300

    pthread para sincronização de threads.
    Criação do README

--- ProjFinal.C
+++ ProjFinal.C
@@ -1,68 +1,69 @@
-#define _GNU_SOURCE#include <stdlib.h>
-#include <malloc.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sched.h>
-#include <stdio.h>
+//Luana Miho Yasuda 42148677
+#define _GNU_SOURCE 
+#include <stdlib.h> 
+#include <malloc.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <signal.h> 
+#include <sched.h> 
+#include <stdio.h> 
+#include <pthread.h>
 
- // 64kB stack
-#define FIBER_STACK 1024 * 64
+// 64kB stack 
+#define FIBER_STACK 1024*64 
 
-struct c {
-  int saldo;
-};
-typedef struct c conta;
+struct c { 
+    int saldo; 
+}; 
 
-conta from, to;
-int valor;
+typedef struct c conta; 
+conta from, to; 
+int valor; 
 
-// The child thread will execute this function
-int transferencia(void * arg) 
-{
-  if (from.saldo >= valor) { // 2
-    from.saldo -= valor;
-    to.saldo += valor;
-  }
-  printf("Transferência concluída com sucesso!\n");
-  printf("Saldo de c1: %d\n", from.saldo);
-  printf("Saldo de c2: %d\n", to.saldo);
-  return 0;
-}
-int main() 
-{
-  void * stack;
-  pid_t pid;
-  int i;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-  // Allocate the stack
-  stack = malloc(FIBER_STACK);
-  if (stack == 0) 
-  {
-    perror("malloc: could not allocate stack");
-    exit(1);
-  }
-
-  // Todas as contas começam com saldo 100 
-  from.saldo = 100;
-  to.saldo = 100;
-
-  printf("Transferindo 10 para a conta c2\n");
-  valor = 10;
-
-  for (i = 0; i < 10; i++) {
-    // Call the clone system call to create the child thread
-    pid = clone( & transferencia, (char * ) stack + FIBER_STACK,
-      SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0);
-    if (pid == -1) {
-      perror("clone");
-      exit(2);
+int transferencia( void *arg) { 
+    pthread_mutex_lock(&mutex);
+    if (from.saldo >= valor){
+        from.saldo -= valor; 
+        to.saldo += valor; 
+    } else {
+        printf("Transferência não foi concluída pois saldo de c1 é 0!\n"); 
+        printf("Saldo de c1: %d\n", from.saldo); 
+        printf("Saldo de c2: %d\n", to.saldo);
+        pthread_mutex_unlock(&mutex);
+        return 0; 
     }
-  }
+    printf("Transferência concluída com sucesso!\n"); 
+    printf("Saldo de c1: %d\n", from.saldo); 
+    printf("Saldo de c2: %d\n", to.saldo); 
+    pthread_mutex_unlock(&mutex);
+    return 0; 
+} 
 
-  // Free the stack
-  free(stack);
-  printf("Transferências concluídas e memória liberada.\n");
-
+int main() { 
+    void* stack; 
+    pid_t pid; 
+    int i; 
+    stack = malloc( FIBER_STACK ); 
+    if ( stack == 0 ) { 
+        perror("malloc: could not allocate stack"); 
+        exit(1); 
+    } 
+    from.saldo = 100; 
+    to.saldo = 100; 
+    printf( "Transferindo 10 para a conta c2\n" ); 
+    valor = 10; 
+    for (i = 0; i < 101; i++) { 
+        pid = clone( &transferencia, (char*) stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 ); 
+        if ( pid == -1 ) { 
+            perror( "clone" ); 
+            exit(2); 
+        } 
+        // Adiciona waitpid aqui para esperar a transação atual ser concluída antes de começar a próxima
+        waitpid(pid, NULL, 0);
+    } 
+    free( stack ); 
+    printf("Transferências concluídas e memória liberada.\n"); 
   return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Proj2-Final
+## luana Miho Yasuda - 42148677
+## Como compilar o programa
+Para compilar o programa, você precisa ter o compilador GCC instalado em sua máquina. Use o seguinte comando para compilar o código:
+
+>gcc -o ProjFinalTransferencia ProjFinal.c -lpthread
+
+O nome final do executavel pode ser mudado, "ProjFinal.c" é o nome do arquivo de codigo fonte.
+O argumento "-lpthread" é usado para ligar a biblioteca de threading do C, ja que agora o codigo usa a biblioteca "pthread".
+
+Apos compilar basta executar o executavel criado na compilação com o seguinte comando:
+
+>./ProjFinalTransferencia
